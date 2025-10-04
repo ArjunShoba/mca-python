232fa5ba87910765a4e795a8d8d6eb71cccdf87e
Arjun S Nair <arjunthengamam0@gmail.com>
2025-10-04 12:10:48 +0530

    simple

---
+++ str_length.py
@@ -0,0 +1,3 @@
+flowers=['rose','jasmin','lotus']
+for i in range(len(flowers)):
+    print("current flower",flowers[i])
---
+++ sum_list.py
@@ -0,0 +1,5 @@
+num=[2,4,6,8,10]
+sum=0
+for i in num:
+    sum=sum+i
+    print("the sum is :",sum)
